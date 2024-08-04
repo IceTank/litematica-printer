00dd9515a19d6622e5fb5fe1d1f0e4e5bdd3973b
Ic3Tank <61137113+IceTank@users.noreply.github.com>
2024-08-04 17:06:57 +0200

    Fix Freelook

--- v1_20_4/src/main/java/me/aleksilassila/litematica/printer/v1_20_4/MovementHandler.java
+++ v1_20_4/src/main/java/me/aleksilassila/litematica/printer/v1_20_4/MovementHandler.java
@@ -3,6 +3,8 @@ package me.aleksilassila.litematica.printer.v1_20_4;
 import me.aleksilassila.litematica.printer.v1_20_4.config.PrinterConfig;
 import me.aleksilassila.litematica.printer.v1_20_4.mixin.MixinAccessorKeyBinding;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.CraftingScreen;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -14,7 +16,9 @@ public class MovementHandler {
 
     public void onGameTick() {
         if (mc.player == null) return;
-        if (!PrinterConfig.FREE_LOOK.getBooleanValue()) {
+
+        // Disabled check
+        if (!PrinterConfig.FREE_LOOK.getBooleanValue() || !LitematicaMixinMod.PRINT_MODE.getBooleanValue()) {
             if (disableNextTick) {
                 disableNextTick = false;
                 InputDirections.apply(InputDirections.getCurrentInput());
@@ -22,16 +26,21 @@ public class MovementHandler {
             return;
         }
 
+        // Disable in inventories
+        if (mc.currentScreen != null) {
+            if (PrinterConfig.MOVE_WHILE_IN_INVENTORY.getBooleanValue()) {
+                if (mc.currentScreen instanceof CraftingScreen || mc.currentScreen instanceof CreativeInventoryScreen) {
+                    return;
+                }
+            } else {
+                return;
+            }
+        }
+
         // Get current inputs
         InputDirections currentInputDirection = InputDirections.getCurrentInput();
-//        if (lastPlayerInputDirection != currentInputDirection) {
-//            currentRotationError = 0;
-//        }
 
         float cameraYaw = mc.gameRenderer.getCamera().getYaw() % 360;
-//        if (axisAlignCamera.getValue()) {
-//            cameraYaw = (float) Math.floor((cameraYaw + 45f) / 90f) * 90;
-//        }
         if (currentInputDirection != InputDirections.NONE) {
             // lastPlayerInputDirection = currentInputDirection;
             // Calculate resulting control direction and apply them
@@ -42,45 +51,15 @@ public class MovementHandler {
             float playerRelativeYaw = inputYaw + playerYaw;
             float resultPlayerYaw = playerRelativeYaw - cameraYaw;
             InputDirections resultDirection = InputDirections.getDirection(resultPlayerYaw);
-            if (resultDirection == null) {
+            if (resultDirection == null || resultDirection == InputDirections.NONE) {
                 // ChatUtil.sendClientMessage("Result direction is null");
+                Printer.logger.warn("Result direction is null / None");
                 return;
             }
 
-            // Relative to player facing
-            float errorDeltaYaw = (resultDirection.getYaw() - resultPlayerYaw) % 360;
-            if (errorDeltaYaw < -180) errorDeltaYaw += 360;
-            if (errorDeltaYaw > 180) errorDeltaYaw -= 360;
-
-//            currentRotationError += errorDeltaYaw;
-//            if (currentRotationError < -180) currentRotationError += 360;
-//            if (currentRotationError > 180) currentRotationError -= 360;
-
-//            InputDirections resultErrorDirection = InputDirections.getDirection(resultPlayerYaw - currentRotationError);
-
-//            if (debugText.getValue()) ChatUtil.sendClientMessage("Error delta: %s (acc: %s)".formatted(errorDeltaYaw, currentRotationError));
-
-            mc.options.forwardKey.setPressed(false);
-            mc.options.backKey.setPressed(false);
-            mc.options.leftKey.setPressed(false);
-            mc.options.rightKey.setPressed(false);
-//            if (resultErrorDirection != resultDirection && attemptErrorCorrection.getValue()) {
-//
-//                float inputErrorYaw = resultErrorDirection.getYaw();
-//                float playerRelativeErrorYaw = inputErrorYaw + playerYaw;
-//                float resultPlayerErrorYaw = playerRelativeErrorYaw - cameraYaw;
-//                float errorErrorDeltaYaw = (InputDirections.getDirection(resultPlayerErrorYaw).getYaw() - resultPlayerErrorYaw) % 360;
-//                if (errorErrorDeltaYaw < -180) errorErrorDeltaYaw += 360;
-//                if (errorErrorDeltaYaw > 180) errorErrorDeltaYaw -= 360;
-//
-//                currentRotationError -= currentRotationError;
-//                if (debugText.getValue()) ChatUtil.sendClientMessage("Choosing error direction ErrorError Delta: %s".formatted(errorErrorDeltaYaw));
-//                InputDirections.apply(resultErrorDirection);
-//            } else {
-                InputDirections.apply(resultDirection);
-            //}
-            // InputDirections.apply(resultDirection);
+            InputDirections.apply(resultDirection);
         } else /*if (overwriteKeys.getValue())*/ {
+//            Printer.logger.info("No input direction");
             InputDirections.apply(InputDirections.NONE);
         }
     }
@@ -105,18 +84,17 @@ public class MovementHandler {
         }
 
         static InputDirections getDirection(float yaw) {
+            while (yaw < 0) yaw += 360;
             yaw = yaw % 360;
-            if (yaw < 0) yaw += 360;
-            if (yaw >= 0 && yaw < 22.5) return FORWARD;
-            if (yaw >= 22.5 && yaw < 67.5) return FORWARD_LEFT;
-            if (yaw >= 67.5 && yaw < 112.5) return LEFT;
-            if (yaw >= 112.5 && yaw < 157.5) return BACK_LEFT;
-            if (yaw >= 157.5 && yaw < 202.5) return BACK;
-            if (yaw >= 202.5 && yaw < 247.5) return BACK_RIGHT;
-            if (yaw >= 247.5 && yaw < 292.5) return RIGHT;
-            if (yaw >= 292.5 && yaw < 337.5) return FORWARD_RIGHT;
-            if (yaw >= 337.5 && yaw < 360) return FORWARD;
-            return NONE;
+            if (yaw < 22.5) return FORWARD;
+            if (yaw < 67.5) return FORWARD_LEFT;
+            if (yaw < 112.5) return LEFT;
+            if (yaw < 157.5) return BACK_LEFT;
+            if (yaw < 202.5) return BACK;
+            if (yaw < 247.5) return BACK_RIGHT;
+            if (yaw < 292.5) return RIGHT;
+            if (yaw < 337.5) return FORWARD_RIGHT;
+            return FORWARD;
         }
 
         /**
@@ -183,6 +161,10 @@ public class MovementHandler {
         }
 
         static void apply(InputDirections direction) {
+            mc.options.forwardKey.setPressed(false);
+            mc.options.leftKey.setPressed(false);
+            mc.options.rightKey.setPressed(false);
+            mc.options.backKey.setPressed(false);
             switch (direction) {
                 case FORWARD -> mc.options.forwardKey.setPressed(true);
                 case FORWARD_LEFT -> {
@@ -205,10 +187,7 @@ public class MovementHandler {
                     mc.options.rightKey.setPressed(true);
                 }
                 case NONE -> {
-                    mc.options.forwardKey.setPressed(false);
-                    mc.options.leftKey.setPressed(false);
-                    mc.options.rightKey.setPressed(false);
-                    mc.options.backKey.setPressed(false);
+
                 }
             }
         }
