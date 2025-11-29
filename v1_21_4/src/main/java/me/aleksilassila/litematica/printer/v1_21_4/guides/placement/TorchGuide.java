80a6e9bd0fc1d2b38e07d1f740fbf1d74fc5c4e7
Ic3Tank <61137113+IceTank@users.noreply.github.com>
2025-11-29 16:12:33 +0100

    Fixup some code

--- v1_21_4/src/main/java/me/aleksilassila/litematica/printer/v1_21_4/guides/placement/TorchGuide.java
+++ v1_21_4/src/main/java/me/aleksilassila/litematica/printer/v1_21_4/guides/placement/TorchGuide.java
@@ -1,12 +1,9 @@
 package me.aleksilassila.litematica.printer.v1_21_4.guides.placement;
 
 import me.aleksilassila.litematica.printer.v1_21_4.SchematicBlockState;
-import me.aleksilassila.litematica.printer.v1_21_4.config.PrinterConfig;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.WallMountedBlock;
-import net.minecraft.state.property.Properties;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.util.math.Direction;
 
@@ -14,7 +11,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-public class TorchGuide extends GuesserGuide {
+public class TorchGuide extends PropertySpecificGuesserGuide {
     public TorchGuide(SchematicBlockState state) {
         super(state);
     }
@@ -33,20 +30,5 @@ public class TorchGuide extends GuesserGuide {
     @Override
     protected Optional<Block> getRequiredItemAsBlock(ClientPlayerEntity player) {
         return Optional.of(state.targetState.getBlock());
-    }
-
-    @Override
-    protected boolean statesEqual(BlockState state1, BlockState state2) {
-        // Redstone torches use LIT which can be false if powered; ignore it to allow placement
-        if (PrinterConfig.PRINTER_IGNORE_ROTATION.getBooleanValue()) {
-            var rotationProps = PropertySpecificGuesserGuide.rotationProperties;
-            // Merge rotation props with LIT
-            net.minecraft.state.property.Property<?>[] merged = new net.minecraft.state.property.Property<?>[rotationProps.length + 1];
-            System.arraycopy(rotationProps, 0, merged, 0, rotationProps.length);
-            merged[rotationProps.length] = Properties.LIT;
-            return statesEqualIgnoreProperties(state1, state2, merged);
-        } else {
-            return statesEqualIgnoreProperties(state1, state2, Properties.LIT);
-        }
     }
 }
