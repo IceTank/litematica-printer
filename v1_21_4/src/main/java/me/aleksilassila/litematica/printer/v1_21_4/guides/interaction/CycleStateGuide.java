0790966a496912a4ef736d16df64a1d3287ca797
nonokirby <82886780+nonokirby@users.noreply.github.com>
2026-01-03 18:16:29 -0500

    Add checks to verify states before changing (#10)
    * Add checks to verify states before changing
    
    Verify that the state that is modified by interaction is not already correct before interacting with the block. Fixes repetitive toggling of blocks when not rotated correctly.
    
    * Remove whitespace

--- v1_21_4/src/main/java/me/aleksilassila/litematica/printer/v1_21_4/guides/interaction/CycleStateGuide.java
+++ v1_21_4/src/main/java/me/aleksilassila/litematica/printer/v1_21_4/guides/interaction/CycleStateGuide.java
@@ -36,6 +36,37 @@ public class CycleStateGuide extends InteractionGuide {
         BlockState targetState = state.targetState;
         BlockState currentState = state.currentState;
 
+        if (currentState.getBlock() == Blocks.REPEATER) {
+            if (currentState.get(RepeaterBlock.DELAY) == targetState.get(RepeaterBlock.DELAY)) {
+                return false;
+            }
+        }
+        if (currentState.getBlock() == Blocks.LEVER) {
+            if (currentState.get(LeverBlock.POWERED) == targetState.get(LeverBlock.POWERED)) {
+                return false;
+            }
+        }
+        if (currentState.getBlock() == Blocks.COMPARATOR) {
+            if (currentState.get(ComparatorBlock.MODE) == targetState.get(ComparatorBlock.MODE)) {
+                return false;
+            }
+        }
+        if (currentState.getBlock() instanceof TrapdoorBlock) {
+            if (currentState.get(TrapdoorBlock.OPEN) == targetState.get(TrapdoorBlock.OPEN)) {
+                return false;
+            }
+        }
+        if (currentState.getBlock() instanceof DoorBlock) {
+            if (currentState.get(DoorBlock.OPEN) == targetState.get(DoorBlock.OPEN)) {
+                return false;
+            }
+        }
+        if (currentState.getBlock() instanceof FenceGateBlock) {
+            if (currentState.get(FenceGateBlock.OPEN) == targetState.get(FenceGateBlock.OPEN)) {
+                return false;
+            }
+        }
+        
         return !statesEqual(targetState, currentState);
     }
 
