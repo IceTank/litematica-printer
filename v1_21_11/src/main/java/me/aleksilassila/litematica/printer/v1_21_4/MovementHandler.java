deb5dfaf03a53e814df19448450679657ef8ed9b
ejeebus <elijahmorgan16514@gmail.com>
2026-06-06 10:27:22 +0000

    Remove EntityAccessor — prevYaw/prevPitch don't exist in Entity in 1.21.11

--- v1_21_11/src/main/java/me/aleksilassila/litematica/printer/v1_21_4/MovementHandler.java
+++ v1_21_11/src/main/java/me/aleksilassila/litematica/printer/v1_21_4/MovementHandler.java
@@ -1,7 +1,6 @@
 package me.aleksilassila.litematica.printer.v1_21_4;
 
 import me.aleksilassila.litematica.printer.v1_21_4.config.PrinterConfig;
-import me.aleksilassila.litematica.printer.v1_21_4.mixin.EntityAccessor;
 import me.aleksilassila.litematica.printer.v1_21_4.mixin.MixinAccessorKeyBinding;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.CraftingScreen;
@@ -71,8 +70,6 @@ public class MovementHandler {
         Vec3d playerPos = new Vec3d(player.getX(), player.getY(), player.getZ());
         mc.getNetworkHandler().sendPacket(new PlayerInputC2SPacket(player.input.playerInput));
         mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.Full(playerPos.x, playerPos.y, playerPos.z, yaw, pitch, player.isOnGround(), player.horizontalCollision));
-        ((EntityAccessor) mc.player).setPrevYaw(yaw);
-        ((EntityAccessor) mc.player).setPrevPitch(pitch);
     }
 
     public void onDisable(ClientPlayerEntity player) {
