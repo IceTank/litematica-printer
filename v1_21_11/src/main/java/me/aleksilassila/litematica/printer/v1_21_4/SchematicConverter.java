a81361b350a1b23969a110f7ada1fa23f6a79b08
ejeebus <elijahmorgan16514@gmail.com>
2026-06-09 06:24:27 +0000

    Fix LitematicaSchematicAccessor for sakura-ryoko litematica 1.21.11
    The constructor signature changed from (File, FileType) to (Path, FileType)
    in the sakura-ryoko litematica fork for 1.21.11. The old @Invoker was causing
    a fatal mixin transformation failure on world join, which manifested as
    "Network Protocol Error" when connecting to 2b2t.

--- v1_21_11/src/main/java/me/aleksilassila/litematica/printer/v1_21_4/SchematicConverter.java
+++ v1_21_11/src/main/java/me/aleksilassila/litematica/printer/v1_21_4/SchematicConverter.java
@@ -5,6 +5,7 @@ import fi.dy.masa.litematica.util.FileType;
 import me.aleksilassila.litematica.printer.v1_21_4.mixin.LitematicaSchematicAccessor;
 
 import java.io.File;
+import java.nio.file.Path;
 
 /**
  * @author IceTank
@@ -12,11 +13,11 @@ import java.io.File;
  */
 public class SchematicConverter {
     public static LitematicaSchematic convertAndReturn(File file, File out) {
-        LitematicaSchematic schematic = LitematicaSchematicAccessor.invokeConstructor(file, FileType.VANILLA_STRUCTURE);
+        LitematicaSchematic schematic = LitematicaSchematicAccessor.invokeConstructor(file.toPath(), FileType.VANILLA_STRUCTURE);
         schematic.readFromFile();
         String fileName = file.getName().replace(".nbt", "");
         schematic.writeToFile(out.toPath(), fileName, true);
-        LitematicaSchematic newSchem = LitematicaSchematicAccessor.invokeConstructor(new File(out, fileName + ".litematic"), FileType.LITEMATICA_SCHEMATIC);
+        LitematicaSchematic newSchem = LitematicaSchematicAccessor.invokeConstructor(out.toPath().resolve(fileName + ".litematic"), FileType.LITEMATICA_SCHEMATIC);
         newSchem.readFromFile();
         return newSchem;
     }
